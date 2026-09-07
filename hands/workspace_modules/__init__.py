1684bfe5b0ec549b80f01d7d605880460faf6562
Manikanta Cheruku <cherukumanikanta77@gmail.com>
2026-09-07 17:21:31 +0530

    feat: create hands/workspace_modules/ package for modular workspace architecture

---
+++ hands/workspace_modules/__init__.py
@@ -0,0 +1,7 @@
+"""
+hands/workspace_modules/
+Modular workspace scanner and restorer.
+Same pattern as main_modules/ and trigger_modules/.
+"""
+from hands.workspace_modules.scanner import scan_current
+from hands.workspace_modules.restore import smart_restore, restore
